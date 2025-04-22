e660b8340d87c8557d8b812e620cd373c847e45c
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-04-22 20:32:03 +0800

    successfully added division from question and quiz option

--- frontend/quiz-creator-v2.0/src/pages/CreateQuizPage.jsx
+++ frontend/quiz-creator-v2.0/src/pages/CreateQuizPage.jsx
@@ -3,13 +3,24 @@ import "../index.css";
 
 const CreateQuizPage = () => {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      <div className="QuizContainer">
-        <p className="Question">Question</p>
+    <div className>
+      <div className="w-[100%] bg-pink-100 h-[60vh]">
+        <p className="content-center">Question</p>
       </div>
-      <button className="bg-[#d9d9d9] rounded-lg px-6 py-2.5 w-[85vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] max-w-[700px] my-5 text-[#213547] font-medium cursor-pointer transition-colors duration-200">
-        Option A
-      </button>
+      <div className="OptionsContainer">
+        <button className="mx-auto block bg-[#d9d9d9] rounded-xl px-6 py-4 w-[85vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] max-w-[700px] my-4 text-[#213547] font-medium cursor-pointer transition-colors duration-200">
+          Option A
+        </button>
+        <button className="mx-auto block bg-[#d9d9d9] rounded-xl px-6 py-4 w-[85vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] max-w-[700px] my-4 text-[#213547] font-medium cursor-pointer transition-colors duration-200">
+          Option B
+        </button>
+        <button className="mx-auto block bg-[#d9d9d9] rounded-xl px-6 py-4 w-[85vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] max-w-[700px] my-4 text-[#213547] font-medium cursor-pointer transition-colors duration-200">
+          Option C
+        </button>
+        <button className="mx-auto block bg-[#d9d9d9] rounded-xl px-6 py-4 w-[85vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] max-w-[700px] my-4 text-[#213547] font-medium cursor-pointer transition-colors duration-200">
+          Option D
+        </button>
+      </div>
     </div>
   );
 };
