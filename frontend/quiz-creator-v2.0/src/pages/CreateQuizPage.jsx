dbd218efaf5025278dfa6b21be6d2eafe15e6406
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-04-22 20:58:21 +0800

    added columns for responsiveness

--- frontend/quiz-creator-v2.0/src/pages/CreateQuizPage.jsx
+++ frontend/quiz-creator-v2.0/src/pages/CreateQuizPage.jsx
@@ -3,23 +3,26 @@ import "../index.css";
 
 const CreateQuizPage = () => {
   return (
-    <div className>
-      <div className="w-[100%] bg-pink-100 h-[60vh]">
-        <p className="content-center">Question</p>
+    <div className="min-h-screen flex flex-col">
+      <div className="w-[100%] bg-[#C3D5D4] h-[50vh] flex items-center justify-center">
+        <p className="text-xl font-medium">Question</p>
       </div>
-      <div className="OptionsContainer">
-        <button className="mx-auto block bg-[#d9d9d9] rounded-xl px-6 py-4 w-[85vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] max-w-[700px] my-4 text-[#213547] font-medium cursor-pointer transition-colors duration-200">
-          Option A
-        </button>
-        <button className="mx-auto block bg-[#d9d9d9] rounded-xl px-6 py-4 w-[85vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] max-w-[700px] my-4 text-[#213547] font-medium cursor-pointer transition-colors duration-200">
-          Option B
-        </button>
-        <button className="mx-auto block bg-[#d9d9d9] rounded-xl px-6 py-4 w-[85vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] max-w-[700px] my-4 text-[#213547] font-medium cursor-pointer transition-colors duration-200">
-          Option C
-        </button>
-        <button className="mx-auto block bg-[#d9d9d9] rounded-xl px-6 py-4 w-[85vw] sm:w-[70vw] md:w-[60vw] lg:w-[50vw] max-w-[700px] my-4 text-[#213547] font-medium cursor-pointer transition-colors duration-200">
-          Option D
-        </button>
+
+      <div className="py-3.5 px-4">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 max-w-[900px] mx-auto">
+          <button className="bg-[#EFEFEF] rounded-xl px-6 py-5 w-full text-[#213547] font-medium cursor-pointer transition-colors duration-200">
+            Option A
+          </button>
+          <button className="bg-[#EFEFEF] rounded-xl px-6 py-5 w-full text-[#213547] font-medium cursor-pointer transition-colors duration-200">
+            Option B
+          </button>
+          <button className="bg-[#EFEFEF] rounded-xl px-6 py-5 w-full text-[#213547] font-medium cursor-pointer transition-colors duration-200">
+            Option C
+          </button>
+          <button className="bg-[#EFEFEF] rounded-xl px-6 py-5 w-full text-[#213547] font-medium cursor-pointer transition-colors duration-200">
+            Option D
+          </button>
+        </div>
       </div>
     </div>
   );
