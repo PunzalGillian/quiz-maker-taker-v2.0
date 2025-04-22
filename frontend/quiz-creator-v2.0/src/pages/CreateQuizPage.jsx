994e660134fca4635f77aecc58fbbca3af631f89
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-04-22 19:34:15 +0800

    made changes to createquiz css and configured the config

--- frontend/quiz-creator-v2.0/src/pages/CreateQuizPage.jsx
+++ frontend/quiz-creator-v2.0/src/pages/CreateQuizPage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const CreateQuizPage = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="QuizContainer">
+        <p className="Question">Question</p>
+      </div>
+      <div className="OptionsContainer">
+        <button className="py-2">Option A</button>
+        <button className="py-2 px-4">Option B</button>
+        <button className="py-2 px-4">Option C</button>
+        <button className="py-2 px-4">Option D</button>
+      </div>
+    </div>
+  );
 };
 
 export default CreateQuizPage;
