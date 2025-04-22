fc8fa5854a3649bbcaad45cad7146f69059a0d1e
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-04-22 21:21:20 +0800

    fixed the responsiveness of quiz-taker ui

--- frontend/quiz-creator-v2.0/src/pages/CreateQuizPage.jsx
+++ frontend/quiz-creator-v2.0/src/pages/CreateQuizPage.jsx
@@ -4,12 +4,12 @@ import "../index.css";
 const CreateQuizPage = () => {
   return (
     <div className="min-h-screen flex flex-col">
-      <div className="w-[100%] bg-[#C3D5D4] h-[50vh] flex items-center justify-center">
+      <div className="w-[100%] bg-[#C3D5D4] lg:h-[60vh] h-[50vh] flex items-center justify-center">
         <p className="text-xl font-medium">Question</p>
       </div>
 
-      <div className="py-3.5 px-4">
-        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 max-w-[900px] mx-auto">
+      <div className="py-7 px-4 flex flex-col lg:py-15">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-4 w-[85vw] max-w-[85vw] mx-auto">
           <button className="bg-[#EFEFEF] rounded-xl px-6 py-5 w-full text-[#213547] font-medium cursor-pointer transition-colors duration-200">
             Option A
           </button>
