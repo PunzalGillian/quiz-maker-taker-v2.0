988fc2244880e7ce00acd431c7c9b5103e1f4185
Gillian Punzal <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-04-23 00:10:34 +0800

    finished ui of quiz creator

--- frontend/quiz-creator-v2.0/src/pages/CreateQuizPage.jsx
+++ frontend/quiz-creator-v2.0/src/pages/CreateQuizPage.jsx
@@ -13,43 +13,46 @@ const CreateQuizPage = () => {
       />
 
       {/* Content overlay */}
-      <div className="relative min-h-screen flex justify-center py-20 lg:py-8">
-        <div className="w-[85vw] px-4 lg:max-w-[600px] rgb[255,255,255,0.5] rounded-lg shadow-lg p-6">
-          <h1 className="font-bold text-xl mb-4">Quiz Maker</h1>
+      <div className="relative min-h-screen flex justify-center py-8 lg:py-8">
+        <div
+          className="w-[85vw] px-8 lg:max-w-[600px] rounded-lg shadow-lg p-6"
+          style={{ backgroundColor: "rgba(255, 255, 255, 0.75)" }}
+        >
+          <h1 className="font-bold text-2xl mb-4">Quiz Maker</h1>
           <p>Quiz Name:</p>
           <input
             placeholder="Enter Quiz Name:"
-            className="w-full px-4 py-3 mb-3 rounded-md border border-gray-300 bg-white"
+            className="w-full px-4 py-2.5 mb-3 rounded-md border border-gray-300 bg-[#EFEFEF]"
           ></input>
           <p>Add a Question:</p>
           <input
             placeholder="Enter a Question:"
-            className="w-full px-4 py-3 mb-3 rounded-md border border-gray-300 bg-white"
+            className="w-full px-4 py-2.5 mb-3 rounded-md border border-gray-300 bg-[#EFEFEF]"
           ></input>
           <p>Option A:</p>
           <input
             placeholder="Enter Option A:"
-            className="w-full px-4 py-3 mb-3 rounded-md border border-gray-300 bg-white"
+            className="w-full px-4 py-2.5 mb-3 rounded-md border border-gray-300 bg-[#EFEFEF]"
           ></input>
           <p>Option B:</p>
           <input
             placeholder="Enter Option B:"
-            className="w-full px-4 py-3 mb-3 rounded-md border border-gray-300 bg-white"
+            className="w-full px-4 py-2.5 mb-3 rounded-md border border-gray-300 bg-[#EFEFEF]"
           ></input>
           <p>Option C:</p>
           <input
             placeholder="Enter Option C:"
-            className="w-full px-4 py-3 mb-3 rounded-md border border-gray-300 bg-white"
+            className="w-full px-4 py-2.5 mb-3 rounded-md border border-gray-300 bg-[#EFEFEF]"
           ></input>
           <p>Option D:</p>
           <input
             placeholder="Enter Option D:"
-            className="w-full px-4 py-3 mb-6 rounded-md border border-gray-300 bg-white"
+            className="w-full px-4 py-2.5 mb-6 rounded-md border border-gray-300 bg-[#EFEFEF]"
           ></input>
-          <button className="w-full px-4 py-3 mb-6 rounded-md border border-gray-300 bg-white font-bold text-2xl">
+          <button className="w-full px-4 py-2.5 mb-6 rounded-md border border-gray-300 bg-[#EFEFEF] font-bold text-2xl">
             +
           </button>
-          <button className="w-full px-4 py-3 rounded-md border border-black text-white bg-[#1B191D]">
+          <button className="w-full px-4 py-2.5 ;g:mb-6 rounded-md border border-black text-white bg-[#1B191D]">
             Create Quiz
           </button>
         </div>
