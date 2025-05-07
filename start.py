5526e799097d56ad96dbc6e247dd4c9ec9fa0e9c
PunzalGillian <gillianrpunzal@iskolarngbayan.pup.edu.ph>
2025-05-07 23:56:46 +0800

    Fix uvicorn import string and Pydantic warning

--- start.py
+++ start.py
@@ -1,6 +1,4 @@
-import os
 import uvicorn
-from backend.app.main import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=10000)
+    uvicorn.run("backend.app.main:app", host="0.0.0.0", port=10000, reload=True)
